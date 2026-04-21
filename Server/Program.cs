803414514e0241f2118536c960d45ef4f00be2ba
junsugi <wnstjrl96@naver.com>
2026-04-21 12:33:53 +0900

    소켓 생성 및 연결테스트 (누락본 추가)#3

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,9 +1,23 @@
-﻿namespace Server;
+﻿using System.Net;
+using ServerCore;
+
+namespace Server;
 
 class Program
 {
+    private static Listener _listener = new Listener();
+    
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        IPEndPoint endPoint = new IPEndPoint(IPAddress.Loopback, 5555);
+        
+        _listener.Init(endPoint);
+        
+        Console.WriteLine("Listening...");
+
+        while (true)
+        {
+            // 서버 종료 방지
+        }
     }
 }
--- ServerCore/Listener.cs
+++ ServerCore/Listener.cs
@@ -1,6 +1,26 @@
-﻿namespace ServerCore;
+﻿using System.Net;
+using System.Net.Sockets;
 
-class Listener
+namespace ServerCore;
+
+public class Listener
 {
+    private Socket _listenSocket;
+    
+    public void Init(IPEndPoint endPoint)
+    {
+        _listenSocket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);    
+        _listenSocket.Bind(endPoint);
+        _listenSocket.Listen(10);
+        
+        SocketAsyncEventArgs args = new SocketAsyncEventArgs();
+        args.Completed += ConnectedTest;
+        
+        _listenSocket.AcceptAsync(args);
+    }
 
+    public void ConnectedTest(object sender, SocketAsyncEventArgs e)
+    {
+        Console.WriteLine("I'm here..");
+    }
 }
