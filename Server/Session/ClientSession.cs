40126c8e8c9a03060855a8ff3f5afd6f98767feb
junsugi <wnstjrl96@naver.com>
2026-04-22 13:39:58 +0900

    초기화 추가 및 무한루프 탈출 추가

--- Server/Session/ClientSession.cs
+++ Server/Session/ClientSession.cs
@@ -1,7 +1,4 @@
-using System.Net;
 using System.Text;
-using Google.Protobuf;
-using Google.Protobuf.Protocol;
 using ServerCore;
 
 namespace Server;
@@ -12,10 +9,6 @@ public class ClientSession : Session
     public override void OnConnected()
     {
         Console.WriteLine("Connected to server");
-
-        byte[] buffer = Encoding.UTF8.GetBytes("Hello Client!");
-        ArraySegment<byte> sendBuffer = new ArraySegment<byte>(buffer);
-        Send(sendBuffer);
     }
 
     public override void OnReceive(ArraySegment<byte> buffer)
@@ -29,7 +22,7 @@ public class ClientSession : Session
             if (buffer.Count > 0)
             {
                 // 헤더 데이터사이즈 (Unit16->2바이트(16비트) 가져와서 0~65535 사이의 정수로 조립해줘)
-                ushort dataSize = BitConverter.ToUInt16(buffer.Array, buffer.Offset);
+                // ushort dataSize = BitConverter.ToUInt16(buffer.Array, buffer.Offset);
                 
                 // 데이터 크기가 버퍼보다 크면 이상한 상태니까
                 // if (buffer.Count < dataSize)
@@ -42,6 +35,10 @@ public class ClientSession : Session
                 Send(buffer);
                 // 무한루프 탈출
                 buffer = new ArraySegment<byte>(buffer.Array, buffer.Offset, 0);
+            }
+            else
+            {
+                break;
             }
         }
     }
