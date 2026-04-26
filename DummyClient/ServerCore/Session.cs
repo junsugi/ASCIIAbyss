ca37cf4027bdbdd6cf723527074c2469967a8fc0
junsugi <wnstjrl96@naver.com>
2026-04-26 16:59:06 +0900

    클라이언트 세션에 회원가입/로그인 핸들 추가 #14

---
+++ DummyClient/ServerCore/Session.cs
@@ -0,0 +1,136 @@
+using System.Net.Sockets;
+using Google.Protobuf;
+
+namespace ServerCore;
+
+public abstract class Session
+{
+    private Socket _socket;
+    private object _lock = new object();
+    private bool _isConnected = true;
+
+    private ArraySegment<byte> _recvBuffer = new ArraySegment<byte>(new byte[65535]);
+
+    private Queue<ArraySegment<byte>> _sendQueue = new Queue<ArraySegment<byte>>();
+    List<ArraySegment<byte>> _pendingList = new List<ArraySegment<byte>>();
+
+    private SocketAsyncEventArgs _recvArgs = new SocketAsyncEventArgs();
+    private SocketAsyncEventArgs _sendArgs = new SocketAsyncEventArgs();
+
+    public abstract void OnConnected();
+    public abstract void OnReceive(ArraySegment<byte> buffer);
+    public abstract void OnSend(ArraySegment<byte> buffer);
+    public abstract void OnDisconnected();
+    
+    public void Start(Socket socket)
+    {
+        _socket = socket;
+
+        _recvArgs.Completed += OnRecvCompleted;
+        _sendArgs.Completed += OnSendCompleted;
+
+        RegisterRecv();
+    }
+
+    private void RegisterRecv()
+    {
+        _recvArgs.SetBuffer(_recvBuffer.Array, _recvBuffer.Offset, _recvBuffer.Count);
+
+        try
+        {
+            bool pending = _socket.ReceiveAsync(_recvArgs);
+            if (!pending)
+                OnRecvCompleted(null, _recvArgs);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+    }
+
+    private void OnRecvCompleted(object? sender, SocketAsyncEventArgs args)
+    {
+        // 소켓에 내용이 있고 Success 일 때
+        if (args.BytesTransferred > 0 && args.SocketError == SocketError.Success)
+        {
+            OnReceive(new ArraySegment<byte>(args.Buffer, args.Offset, args.BytesTransferred));
+
+            RegisterRecv();
+        }
+        else
+        {
+            Disconnected();
+        }
+    }
+
+    private void RegisterSend()
+    {
+        // 이미 Socket이 파괴됐는데 들어와서 sendArgs를 등록하려는 경우가 존재
+        // 커넥션이 끊어졌으면 그냥 되돌아가게 하기.
+        if (_isConnected == false)
+            return;
+
+        while (_sendQueue.Count > 0)
+        {
+            ArraySegment<byte> buffer = _sendQueue.Dequeue();
+            _pendingList.Add(buffer);
+        }
+
+        _sendArgs.BufferList = _pendingList;
+
+        try
+        {
+            bool pending = _socket.SendAsync(_sendArgs);
+            if (!pending)
+                OnSendCompleted(null, _sendArgs);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+        }
+    }
+
+    private void OnSendCompleted(object? sender, SocketAsyncEventArgs args)
+    {
+        lock (_lock)
+        {
+            if (args.BytesTransferred > 0 && args.SocketError == SocketError.Success)
+            {
+                _sendArgs.BufferList = null;
+                _pendingList.Clear();
+                
+                if (_sendQueue.Count > 0)
+                    RegisterSend();
+            }
+            else
+            {
+                Disconnected();
+            }
+        }
+    }
+
+    public void Send(ArraySegment<byte> buffer)
+    {
+        // 보낼 데이터 없음 끝
+        if (buffer.Count == 0)
+            return;
+
+        lock (_lock)
+        {
+            _sendQueue.Enqueue(buffer);
+            if (_pendingList.Count == 0)
+                RegisterSend();
+        }
+    }
+
+    private void Disconnected()
+    {
+        if (Interlocked.Exchange(ref _isConnected, false) == false)
+            return;
+        
+        OnDisconnected();
+        Console.WriteLine($"{GetType().Name} disconnected");
+        _socket.Shutdown(SocketShutdown.Both);
+        _socket.Close();
+    }
+}
