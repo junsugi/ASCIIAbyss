a907792847146577ebad07f028e56de6c8018d64
junsugi <wnstjrl96@naver.com>
2026-04-27 10:55:25 +0900

    서버 방 생성/리스트 로직 추가 #12

--- Server/Session/ClientSession_PreGame.cs
+++ Server/Session/ClientSession_PreGame.cs
@@ -1,11 +1,13 @@
 using Google.Protobuf.Protocol;
 using Server.Game;
 using ServerCore;
+using Player = Server.Game.Player;
 
 namespace Server;
 
 public partial class ClientSession : PacketSession
 {
+    // TODO: 패킷을 몰라야한다.
     public void HandleSignUp(C_SignUp packet)
     {
         // 회원가입 시작
@@ -16,6 +18,7 @@ public partial class ClientSession : PacketSession
             Send(new S_SignUp());
     }
 
+    // TODO: 패킷을 몰라야한다.
     public void HandleSignIn(C_SignIn packet)
     {
         string email = packet.AccountInfo.Email;
@@ -25,14 +28,42 @@ public partial class ClientSession : PacketSession
         if (result != null)
         {
             S_SignIn signInPacket = new S_SignIn();
-            signInPacket.Player = result.Player.Mapper();
+            signInPacket.Player = result.Player.MapperToProto();
             Send(signInPacket);
         }
     }
+
+    public void HandleMakeGameRoom(string displayName)
+    {
+        GameRoom gameRoom = GameRoomManager.Instance.MakeGameRoom(displayName);
+        if (gameRoom != null)
+        {
+            // TODO: 클라에서 방 입장 시키기
+            S_CreateGameRoom createGamePacket = new S_CreateGameRoom();
+            createGamePacket.GameRoom = gameRoom.MapperToProto(gameRoom);
+            Send(createGamePacket);
+        }
+    }
     
+    public void HandleGameRoomList()
+    {
+        Dictionary<int, GameRoom> gameRooms = GameRoomManager.Instance.FindAll();
+        // TODO: 클라한테 방 정보 전송
+        S_GameRoomList gameRoomListPacket = new S_GameRoomList();
+        gameRoomListPacket.GameRoom.AddRange(gameRooms.Values.Select(gameRoom => gameRoom.MapperToProto(gameRoom)));
+        Send(gameRoomListPacket);
+    }
+    
+    // TODO: 패킷을 몰라야한다.
     public void HandleEnterGame(C_EnterGame enterGamePacket)
     {
         // 게임 룸에 입장
-        GameRoom room = GameRoomManager.Instance.Find(1);
+        Player player = new Player();
+        player = player.MapperToPlayer(enterGamePacket.Player);
+        
+        int roomId = enterGamePacket.RoomId;
+        GameRoom room = GameRoomManager.Instance.Find(roomId);
+        if (room != null)
+            room.EnterGame(roomId, player);
     }
 }
